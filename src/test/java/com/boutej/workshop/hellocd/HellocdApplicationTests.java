cf0818faa73dfce97d1a73d0498120f389896ccd
saraBoutej <sboutej@exoplatform.com>
2023-11-24 15:36:24 +0100

    adding files created with spring boot generator

---
+++ src/test/java/com/boutej/workshop/hellocd/HellocdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boutej.workshop.hellocd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellocdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
